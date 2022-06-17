72112aa29f670725e75ccc7a6b625ad967eaa122
Omarhuba <omaruba@gmail.com>
2022-06-17 09:49:43 +0200

    added som pics in assets

---
+++ src/assets/logos/projectsdata.js
@@ -0,0 +1,49 @@
+const projects = [
+  {
+    id: 5,
+    name: 'geoQuizzr',
+    imgURL: 'geoquizzr',
+    desc:
+      'A full stack map-based quiz app where users answer questions based on their location and collect points for correct answers. The front end is powered by React, Redux and Typescript which communicates with a Node/Express API and MongoDB database.',
+    codelink: 'https://github.com/emmalearnscode/gq-frontend',
+    sitelink: 'https://geoquizzr.netlify.app/',
+  },
+  {
+    id: 4,
+    name: 'MeetApp',
+    imgURL: 'meetapp',
+    desc:
+      'A mock version of meetup.com. Users can find events in their local area, login, register and unregister for events, chat, leave a rating and create their own events. The site is built using React, Redux and Typescript using Test Driven Development (Jest and React Testing Library).',
+    codelink: 'https://github.com/emmalearnscode/tdd-meetup',
+    sitelink: 'https://tddmeetapp.netlify.app/',
+  },
+  {
+    id: 3,
+    name: 'Företagspoolen ITHS Stockholm',
+    imgURL: 'foretagspoolen',
+    desc:
+      'A searchable, sortable resource for ITHS Stockholm students to search for internships. The site is built in React and Firebase and also has a password-protected admin interface for staff to keep the internship pool updated.',
+    codelink: 'https://github.com/emmalearnscode/foretagspoolen',
+    sitelink: 'https://youtu.be/qUmTN5Ailc8',
+  },
+  {
+    id: 2,
+    name: 'Sinus Webshop',
+    imgURL: 'sinus-webshop',
+    desc:
+      'An e-commerce store built in Vue JS with VueX and routing. Customers can create an account to save their information and there is also an admin interface to create, read, update and delete products and orders.',
+    codelink: 'https://github.com/emmalearnscode/sinus-webshop',
+    sitelink: 'https://youtu.be/rikf5ZNmD6I',
+  },
+  {
+    id: 1,
+    name: 'Beer Wiki',
+    imgURL: 'beer-wiki',
+    desc:
+      'A single page application where you can search for a variety of beers, created using the punkIPA API, written in HTML, CSS and JavaScript. It is possible to acess a randomly chosen beer, search simply by name or do an advanced search. The site is fully mobile-responsive.',
+    codelink: 'https://github.com/emmalearnscode/js10-vanilla-slutprojekt',
+    sitelink: 'https://emmalearnscode.github.io/js10-vanilla-slutprojekt/',
+  },
+]
+
+export default projects;
